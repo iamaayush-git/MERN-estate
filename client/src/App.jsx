3f6b9a4ac67567e443c69f68bab5c65944fe4c4d
Aayush Bhattarai <aayushbhattaraia6@gmail.com>
2024-12-20 17:56:45 +0545

    initialized the project

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="">
+      <h2 className="">This is the best websitefdfd by mine.</h2>
+    </div>
+  );
+};
+
+export default App;
