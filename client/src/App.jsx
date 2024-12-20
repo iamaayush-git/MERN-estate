0556c5c056d3dfbacd2ffc4a7b5db2fba2de1f59
Aayush Bhattarai <aayushbhattaraia6@gmail.com>
2024-12-20 18:05:48 +0545

    pages and routes created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
 const App = () => {
   return (
-    <div className="">
-      <h2 className="">This is the best websitefdfd by mine.</h2>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignOut />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
